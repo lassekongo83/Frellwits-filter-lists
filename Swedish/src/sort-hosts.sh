2d3f03cc674ead137c8360965f26edf63eb45ee5
lassekongo83 <lassekongo83@users.noreply.github.com>
2018-09-23 19:51:40 +0200

    Add Swedish hosts file

---
+++ Swedish/src/sort-hosts.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Homepage: https://github.com/lassekongo83/Frellwit-s-filter-lists
+# Description: Very simple script to sort the hosts file and include the headers
+# License: GPLv3
+
+date=$(date '+%Y-%m-%d %H:%M:%S %Z')
+
+cat main-hosts.txt | sort > output/sorted-hosts.txt
+
+cat > output/hosts-headers.txt <<EOF
+# Server list for use with hosts files to block ads, tracking, scams & badware on (mostly) Swedish websites
+# ----
+# last updated:   ${date}
+# format:         hosts (hosts -- in hosts file format)
+# homepage:       https://github.com/lassekongo83/Frellwits-filter-lists/
+EOF
+
+cat output/hosts-headers.txt output/sorted-hosts.txt > ../../Frellwits-Swedish-Hosts-File.txt
+
+rm output/*.txt
