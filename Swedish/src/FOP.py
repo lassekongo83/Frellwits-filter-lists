5c26c64d2a6cae8b57f506e6b442cf7b115987c8
lassekongo83 <lassekongo83@users.noreply.github.com>
2019-08-15 05:36:35 +0200

    Add more options

--- Swedish/src/FOP.py
+++ Swedish/src/FOP.py
@@ -62,6 +62,10 @@ KNOWNOPTIONS = ("collapse", "csp", "document", "elemhide",
                 "redirect=google-analytics.com/cx/api.js", "redirect=googlesyndication.com/adsbygoogle.js", "redirect=doubleclick.net/instream/ad_status.js", 
                 "redirect=ampproject.org/v0.js", "redirect=noopjs", "redirect=noopcss", "redirect=noopframe", "redirect=nooptext", 
                 "redirect=noopmp3-0.1s", "redirect=noopmp4-1s", "redirect=1x1-transparent.png", "redirect=2x2-transparent.png", 
+                "redirect=google-analytics_ga.js", "redirect=google-analytics_analytics.js", "redirect=googletagservices_gpt.js", 
+                "redirect=google-analytics_cx_api.js", "redirect=googlesyndication_adsbygoogle.js", "redirect=doubleclick_instream_ad_status.js", 
+                "redirect=ampproject_v0.js", "redirect=noop.js", "redirect=noop.css", "redirect=noop.html", "redirect=noop.txt", 
+                "redirect=noop-0.1s.mp3", "redirect=noop-1s.mp4", "redirect=1x1.gif", "redirect=2x2.png", "redirect=3x2.png", "redirect=32x32.png", 
                 "redirect=3x2-transparent.png", "redirect=32x32-transparent.png", "1p", "3p", "inline-script", "xhr", "mp4", "empty", "all")
 
 # List the supported revision control system commands
